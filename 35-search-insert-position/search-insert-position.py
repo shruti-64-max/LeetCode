50cdf0aa75af2af1d00316bae3095b26be3fb232
shruti-64-max <dasguptashruti615@gmail.com>
2026-08-11 13:22:19 +0530

    Time: 0 ms (100.00%) | Memory: 13 MB (30.13%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        l,m=0,0
+        r=len(nums)-1
+        while l<=r:
+            m=(l+r)//2
+            if nums[m]==target:
+                return m
+            elif nums[m]<target:
+                l=m+1
+            else:
+                r=m-1
+        return l
